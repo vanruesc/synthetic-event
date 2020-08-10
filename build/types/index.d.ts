ab85c8a3ffacab5d67c30857947a647eedf59cad
Raoul v. R <vanruesc@users.noreply.github.com>
2020-08-10 18:10:14 +0200

    Fix declarations

---
+++ build/types/index.d.ts
@@ -0,0 +1,4 @@
+export { Event } from "./Event";
+export { EventListener } from "./EventListener";
+export { EventListenerObject } from "./EventListenerObject";
+export { EventTarget } from "./EventTarget";
--- build/types/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { Event } from "./Event";
-export { EventTarget } from "./EventTarget";
